7699cba113b610a698c8abe2aca2337ba0f7d29b
Tushar Chokshi <tushar.chokshi@cdk.com>
2017-10-01 14:14:35 -0700

    enhancing lambda function to insert record in DynamoDB

--- src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -1,33 +1,59 @@
 package com.amazonaws.lambda.demo;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-// You can write python and node.js code for lambda function directly in aws console, but you cannot do the same with java code.
-// For Java code based lambda, you need to implement Java code in editor and then upload a jar file in aws console.
+/*
+Information about Building Lambda Function
+http://docs.aws.amazon.com/lambda/latest/dg/java-programming-model.html
 
-// AWS Lambda supports input/output of JSON-serializable types and InputStream/OutputStream types. 
-// Logging: 
-// Context uses LambdaLogger. You can access it as shown below from context object. It writes the log events to CloudWatch.
-// LambdaLogger logger = context.getLogger();
-// logger.log("received : " + myCount);
+You can write python and node.js code for lambda function directly in aws console, but you cannot do the same with java code.
+For Java code based lambda, you need to implement Java code in editor and then upload a jar file in aws console.
 
-// Context object
-// http://docs.aws.amazon.com/lambda/latest/dg/java-context-object.html
+AWS Lambda supports input/output of JSON-serializable types and InputStream/OutputStream types.
 
-// Sample lambda java function that takes json as input and outputs a string
-// right click on the project and run this function. This function will be deployed to your AWS account and then it will be run.
-// while deploying, it may ask you to provide certain information like Role (what permissions this lambda function should have).
-// If you do not have any role already created, you can let it create a default one. 
-// You can also define max time for which this lambda function should run (default is 15 seconds). You can also mention memory (default 512 MB) requires by this lambda function.
-// You use context.getRemainingTimeInImills() to know how much time remained from those 15 seconds after this lambda function was run.  
+Logging: 
+Context uses LambdaLogger. You can access it as shown below from context object. It writes the log events to CloudWatch.
+LambdaLogger logger = context.getLogger();
+logger.log("received : " + myCount);
 
-// http://docs.aws.amazon.com/lambda/latest/dg/java-programming-model.html
-// input: {"firstName":"Tushar","lastName":"Chokshi"}
-public class LambdaFunctionHandler implements RequestHandler<RequestClass, String> {
+Context object
+http://docs.aws.amazon.com/lambda/latest/dg/java-context-object.html
+
+Here is the Sample lambda java function that takes json as input and outputs a json.
+Right click on the project and run this function. This function will be deployed to your AWS account and then it will be run.
+While deploying, it may ask you to provide certain information like Role (what permissions this lambda function should have).
+If you do not have any role already created, you can let it create a default one. But for this code, it will require access to DynamoDB. So, create a role that has full access to DynamoDB (or at least get/put/list access to DynamoDB).
+You can also define max time for which this lambda function should run (default is 15 seconds). You can also mention memory (default 512 MB) requires by this lambda function.
+You use context.getRemainingTimeInImills() to know how much time remained from those 15 seconds after this lambda function was run.  
+
+This function is creating a record in DynamoDB. For that, either this function needs to access DynamoDB using access credentials or you need to create and assign a Role to this function that can access DynamoDB.
+
+Integrating with AWS API Gateway:
+	Create an API with POST. As an integrated resource, use this lambda function.
+	If you want, you can create a stage for this API and get the API link that you can hit through browser or you can just use testing tool of aws console.
+
+input: {"firstName":"Tushar","lastName":"Chokshi"}
+	
+*/
+public class LambdaFunctionHandler implements RequestHandler<RequestClass, ResponseClass> {
 
     @Override
-    public String handleRequest(RequestClass input, Context context) {
+    public ResponseClass handleRequest(RequestClass input, Context context) {
     	
     		// You can see System.out, System.err, logger logs in CloudWatch.
         context.getLogger().log("Input: " + input.toString());
@@ -38,8 +64,43 @@ public class LambdaFunctionHandler implements RequestHandler<RequestClass, Strin
         System.out.println("CloudWatch log stream name: " + context.getLogStreamName());
         System.out.println("CloudWatch log group name: " + context.getLogGroupName());
         
-        // TODO: implement your handler
-        return "Hello from Lambda to "+ input.toString();
+        
+        DynamoDB dynamoDB = getDynamoDbHandler();
+
+        Table table = dynamoDB.getTable("person");
+        
+        String id = "1";
+        try {
+            System.out.println("Adding a new item...");
+            PutItemOutcome outcome = table
+                    .putItem(
+                            new Item()
+                                    .withPrimaryKey("id", id)
+                                    .with("firstName", input.getFirstName())
+                                    .with("lastName", input.getLastName()));
+
+            System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        
+        ResponseClass res = new ResponseClass();
+        res.setId(id);
+        res.setRequestClass(input);
+        
+        return res;
     }
 
+    protected static DynamoDB getDynamoDbHandler() {
+	    	// you don't need to set this credentials as this lambda function is assigned a role that can access DynamoDB.
+        //AWSCredentials awsCredentials = new BasicAWSCredentials(Credentials.access_key_id, Credentials.secret_access_key);
+        AmazonDynamoDB client = new AmazonDynamoDBClient();
+        client.setRegion(Region.getRegion(Regions.US_WEST_2));
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        return dynamoDB;
+
+    }
 }
---
+++ src/main/java/com/amazonaws/lambda/demo/ResponseClass.java
@@ -0,0 +1,25 @@
+package com.amazonaws.lambda.demo;
+
+public class ResponseClass {
+
+	private String id;
+	
+	private RequestClass requestClass;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public RequestClass getRequestClass() {
+		return requestClass;
+	}
+
+	public void setRequestClass(RequestClass requestClass) {
+		this.requestClass = requestClass;
+	}
+
+}
